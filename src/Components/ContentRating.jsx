9cad2a2ec8e42ebf6fcbeb65bd3fa577eaeeb143
Michael Congo <77070513+byCongo@users.noreply.github.com>
2024-09-01 12:50:22 +0000

    Added user interactions like and dislike

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,18 +1,36 @@
 
 import React, { Component } from 'react';
 import './ContentRating.css';
+import { render } from 'react-dom';
 
-class ContentRating extends Component {
-  constructor() {
-    super();
+class ContentRating extends React.Component
+  constructor(props) {
+    super(props);
+
+    // Initialize the state
+    this.state = {
+      likes: 0,
+      dislikes: 0
+    };
   }
+
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+      // The UI structure
+      <div className='content-rating'>
+          <p>
+            Do you love me baby, or do you love my money
+          </p>
+          <div className='rating-buttons'>
+            <button className='like-button'>
+              Like ({this.state.likes})
+            </button>
+            <button className='dislike-button'>
+              Dislike ({this.state.dislikes})
+            </button>
+          </div>
+      </div> 
     );
   }
-}
 
 export default ContentRating;
