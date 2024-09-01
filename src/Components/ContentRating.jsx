c9d6facc28db6eb450d8f4fd8b2c4ae0492e2b5a
Michael Congo <77070513+byCongo@users.noreply.github.com>
2024-09-01 13:39:15 +0000

    content rating done

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -3,14 +3,26 @@ import React, { Component } from 'react';
 import './ContentRating.css';
 import { render } from 'react-dom';
 
-class ContentRating extends React.Component
-  constructor(props) {
-    super(props);
-
-    // Initialize the state
+class ContentRating extends React.Component {
+  constructor() {
+    super();
     this.state = {
       likes: 0,
-      dislikes: 0
+      dislikes: 0,
+
+      // Event handler for the like button
+      handleLike: () => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1
+        }));
+      },
+
+      // Event handler for the dislike button
+      handleDislike: () => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1
+        }));
+      }
     };
   }
 
@@ -22,15 +34,17 @@ class ContentRating extends React.Component
             Do you love me baby, or do you love my money
           </p>
           <div className='rating-buttons'>
-            <button className='like-button'>
+            <button className='like-button' onClick={this.state.handleLike}>
               Like ({this.state.likes})
             </button>
-            <button className='dislike-button'>
+            <button className='dislike-button' onClick={this.state.handleDislike}>
               Dislike ({this.state.dislikes})
             </button>
           </div>
       </div> 
     );
-  }
+  };
+}
+
 
 export default ContentRating;
